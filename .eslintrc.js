a2dd0b35bd9dc111b9d11150d722132b2053fab2
Le Khac Man <lekhacman@outlook.com>
2022-10-16 11:32:20 +0800

    format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,28 @@
 module.exports = {
-  extends: ['eslint:recommended', 'prettier', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
   env: {
     node: true,
     browser: true
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
   plugins: ['prettier', '@typescript-eslint'],
   root: true,
-  "rules": {
-    "prettier/prettier": [
-      "error",
+  'rules': {
+    'prettier/prettier': [
+      'error',
       {
-        "singleQuote": true,
-        "trailingComma": "es5"
+        'singleQuote': true,
+        'trailingComma': 'es5'
       }
     ]
   }
-}
+};
